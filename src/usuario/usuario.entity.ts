e9dd985672c4c5fcc26644cc75c27b67db2148c2
Robson <lprobson@outlook.com>
2022-01-19 18:10:16 -0300

    uso de interceptadores

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,14 +1,24 @@
+import { Exclude, Expose } from "class-transformer";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsNomeDeUsuarioUnico } from "./is-nome-de-usuario-unico";
 
 export class Usuario {
     id: number;
 
-
-    @IsString()
+    @Expose({
+        name: 'username'
+    })
+    @IsNomeDeUsuarioUnico({
+        message: 'nome do usuario precisa ser unico'
+    })
+    @IsString({
+        message: 'usuario precisa ser uma string'
+    })
     @IsNotEmpty({
         message: 'nome é um campo obrigatorio'
     })
     nome: string;
+
 
     @IsEmail()
     @IsNotEmpty({
@@ -16,10 +26,20 @@ export class Usuario {
     })
     email: string;
 
+    @Expose({
+        name: 'password'
+    })
+    @Exclude({
+        toPlainOnly: true
+    })
     @IsNotEmpty({
         message: 'senha é um campo obrigatorio'
     })
     senha: string;
 
+
+    @Expose({
+        name: 'joinDate'
+    })
     dataCadastro: Date;
 }
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { IsNomedeUsuarioUnicoConstraint } from "./is-nome-de-usuario-unico";
 import { UsuarioController } from "./usuario.controller";
 import { UsuarioService } from "./usuario.service";
 
 @Module({
     controllers: [UsuarioController],
-    providers: [UsuarioService]
+    providers: [UsuarioService, IsNomedeUsuarioUnicoConstraint]
 })
 export class UsuarioModule {
     
