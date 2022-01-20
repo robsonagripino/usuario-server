e745540e7b2771004904753fe090ecb35326b49b
Robson <lprobson@outlook.com>
2022-01-20 09:49:09 -0300

    adicionando error 404 e finalizacao do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FiltroDeExcecaoHttp } from './common/filtro/filtro-de-excecao-http.filter';
+import { TransformaRespostaInterceptor } from './core/http/transformar-resposta-interceptor';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
@@ -16,6 +17,9 @@ import { UsuarioModule } from './usuario/usuario.module';
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor
+    },{
+      provide: APP_INTERCEPTOR,
+      useClass: TransformaRespostaInterceptor
     }],
 })
 export class AppModule {}
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post } from "@nestjs/common";
+import { NotFoundError } from "rxjs";
+import { NestResponse } from "src/core/http/nest-response";
+import { NestResponseBuild } from "../core/http/nest-response-build";
 import { Usuario } from "./usuario.entity";
 import { UsuarioService } from "./usuario.service";
 
@@ -10,13 +13,35 @@ export class UsuarioController {
     @Get(':nome')
     public buscaPorUsuario(@Param('nome') nome) {
         const usuarioEncontrado = this.usuarioService.buscaPorUsuario(nome);
+        
+        if(!usuarioEncontrado) {
+            throw new NotFoundException({
+                statusCode: HttpStatus.NOT_FOUND,
+                message: 'Usuário não encontrado.'
+            });
+        }
+        
         return usuarioEncontrado;
     }
 
     @Post()
-    public cria(@Body() usuario: Usuario): Usuario {
+    public cria(@Body() usuario: Usuario): NestResponse {
         //throw new Error('Erro no cadastro do usuario');
         const usuarioCriado = this.usuarioService.cria(usuario);
-        return usuarioCriado;
+        
+        return new NestResponseBuild()
+            .comStatus(HttpStatus.CREATED)
+            .comHeaders({
+                'Location': `/users/${usuarioCriado.nome}`
+            })
+            .comBody({
+                usuarioCriado
+            })
+            .build();
+        // res.status(HttpStatus.CREATED)
+        //     .location(`/users/${usuarioCriado.nome}`)
+        //     .json(usuarioCriado);
+
+        //return usuarioCriado;
     }
 }
